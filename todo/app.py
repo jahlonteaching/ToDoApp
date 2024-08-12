975a8b6ab0fe7d14a3c816e50634139298a614da
Jesus Andres Hincapie <jahlon@gmail.com>
2024-08-12 18:20:15 -0500

    Create main function

--- todo/app.py
+++ todo/app.py
@@ -2,7 +2,11 @@ from todo.model.todo import TodoBook
 from todo.view.ui_console import Console
 
 
-if __name__ == "__main__":
+def main():
     book: TodoBook = TodoBook()
     ui: Console = Console(book)
     ui.app_loop()
+
+
+if __name__ == "__main__":
+    main()
